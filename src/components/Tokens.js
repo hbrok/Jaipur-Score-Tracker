676bea1ad63b3b7db1d44589ae4c497799059934
Heather B <heatherbrokmeier@gmail.com>
2019-01-05 14:48:28 -0600

    Add icons to tokens
    * Add icons for each goods token type
    * Update readme with icon attribution

--- src/components/Tokens.js
+++ src/components/Tokens.js
@@ -1,15 +1,31 @@
 import React, { Component } from "react";
 import "./Tokens.css";
-import Diamond from "icons/Diamond";
+
+import Diamond from "./../icons/Diamond";
+import Cloth from "./../icons/Cloth";
+import Gold from "./../icons/Gold";
+import Leather from "./../icons/Leather";
+import Spice from "./../icons/Spice";
+import Silver from "./../icons/Silver";
+
+const components = {
+  cloth: Cloth,
+  gold: Gold,
+  leather: Leather,
+  spice: Spice,
+  silver: Silver,
+  diamond: Diamond
+};
 
 class Token extends Component {
   renderTokens() {
     const { name, values, onClick } = this.props;
+    const Icon = components[name];
 
     return values.map((value, i) => {
       return (
         <div key={i} className={`token -${name}`} onClick={onClick}>
-          {name}
+          <Icon />
 
           <div className="token-value -right">{value}</div>
           <div className="token-value -left">{value}</div>
