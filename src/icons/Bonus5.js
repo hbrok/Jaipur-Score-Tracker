da1f2906079267c55c0de1a3d6308ac3f0d78b6c
Heather B <heatherbrokmeier@gmail.com>
2019-01-25 12:00:36 -0600

    Refactor bonus icons
    Remove stroke colors, stroke-width, and unnecceary groupa

--- src/icons/Bonus5.js
+++ src/icons/Bonus5.js
@@ -2,13 +2,11 @@ import React from "react";
 
 const Cloth = props => (
   <svg viewBox="0 0 325 315">
-    <g id="5" stroke="#000000" stroke-width="5">
-      <rect id="Rectangle" x="229.5" y="168.5" width="93" height="144" rx="8" />
-      <rect id="Rectangle" x="2.5" y="168.5" width="93" height="144" rx="8" />
-      <rect id="Rectangle" x="2.5" y="2.5" width="93" height="144" rx="8" />
-      <rect id="Rectangle" x="229.5" y="2.5" width="93" height="144" rx="8" />
-      <rect id="Rectangle" x="116.5" y="77.5" width="93" height="144" rx="8" />
-    </g>
+    <rect id="Rectangle" x="229.5" y="168.5" width="93" height="144" rx="8" />
+    <rect id="Rectangle" x="2.5" y="168.5" width="93" height="144" rx="8" />
+    <rect id="Rectangle" x="2.5" y="2.5" width="93" height="144" rx="8" />
+    <rect id="Rectangle" x="229.5" y="2.5" width="93" height="144" rx="8" />
+    <rect id="Rectangle" x="116.5" y="77.5" width="93" height="144" rx="8" />
   </svg>
 );
 
